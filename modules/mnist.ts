e767b52feb397ea5280b7ada88f52f359779d765
David Veszelovszki <veszelovszki@gmail.com>
2023-03-22 14:07:22 +0100

    Convert to async

--- modules/mnist.ts
+++ modules/mnist.ts
@@ -1,5 +1,3 @@
-// utils/mnist.ts
-
 import fs from 'fs';
 import { Buffer } from 'buffer';
 
@@ -7,18 +5,18 @@ const readInt32 = (buffer: Buffer, offset: number) => {
     return buffer.readInt32BE(offset);
 };
 
-const readImages = (filePath: string): MnistImage[] => {
-    const buffer = fs.readFileSync(filePath);
-    const magicNumber = readInt32(buffer, 0);
-    const numImages = readInt32(buffer, 4);
-    const numRows = readInt32(buffer, 8);
-    const numCols = readInt32(buffer, 12);
+const readImages = async (filePath: string): Promise<MnistImage[]> => {
+    const buffer = await fs.promises.readFile(filePath)
+    const magicNumber = readInt32(buffer, 0)
+    const numImages = readInt32(buffer, 4)
+    const numRows = readInt32(buffer, 8)
+    const numCols = readInt32(buffer, 12)
 
     if (magicNumber !== 2051) {
-        throw new Error('Invalid image file magic number');
+        throw new Error('Invalid image file magic number')
     }
 
-    const images = [];
+    const images = []
 
     for (let i = 0; i < numImages; i++) {
         const image = [];
@@ -36,16 +34,16 @@ const readImages = (filePath: string): MnistImage[] => {
     return images;
 };
 
-const readLabels = (filePath: string): MnistLabel[] => {
-    const buffer = fs.readFileSync(filePath);
-    const magicNumber = readInt32(buffer, 0);
-    const numLabels = readInt32(buffer, 4);
+const readLabels = async (filePath: string): Promise<MnistLabel[]> => {
+    const buffer = await fs.promises.readFile(filePath)
+    const magicNumber = readInt32(buffer, 0)
+    const numLabels = readInt32(buffer, 4)
 
     if (magicNumber !== 2049) {
-        throw new Error('Invalid label file magic number');
+        throw new Error('Invalid label file magic number')
     }
 
-    const labels = [];
+    const labels = []
 
     for (let i = 0; i < numLabels; i++) {
         const label = buffer.readUInt8(8 + i);
@@ -62,21 +60,21 @@ export type MnistImageAndLabel = {
     images: MnistImage[],
     labels: MnistLabel[],
 }
-type MnistData = {
+export type MnistData = {
     train: MnistImageAndLabel,
     test: MnistImageAndLabel
 }
 
-export const loadMNIST = (dataPath: string): MnistData => {
-    const trainImagesPath = `${dataPath}/train-images-idx3-ubyte`;
-    const trainLabelsPath = `${dataPath}/train-labels-idx1-ubyte`;
-    const testImagesPath = `${dataPath}/t10k-images-idx3-ubyte`;
-    const testLabelsPath = `${dataPath}/t10k-labels-idx1-ubyte`;
+export const loadMNIST = async (dataPath: string): Promise<MnistData> => {
+    const trainImagesPath = `${dataPath}/train-images-idx3-ubyte`
+    const trainLabelsPath = `${dataPath}/train-labels-idx1-ubyte`
+    const testImagesPath = `${dataPath}/t10k-images-idx3-ubyte`
+    const testLabelsPath = `${dataPath}/t10k-labels-idx1-ubyte`
 
-    const trainImages = readImages(trainImagesPath);
-    const trainLabels = readLabels(trainLabelsPath);
-    const testImages = readImages(testImagesPath);
-    const testLabels = readLabels(testLabelsPath);
+    const trainImages = await readImages(trainImagesPath)
+    const trainLabels = await readLabels(trainLabelsPath)
+    const testImages = await readImages(testImagesPath)
+    const testLabels = await readLabels(testLabelsPath)
 
     return {
         train: {
